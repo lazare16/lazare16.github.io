f4f8ce36c393c396b7439d78eb2498fd6821e7d5
lazare16 <76008900+lazare16@users.noreply.github.com>
2022-12-17 04:16:46 +0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,7 +3,17 @@ function myFunction(x) {
     var x = document.getElementById("mobile-nav");
     if (x.style.display === "block") {
       x.style.display = "none";
+      x.style.width = "0";
     } else {
       x.style.display = "block";
+      x.style.width = "250px";
     }
   }
+
+//   function openNav() {
+//   document.getElementById("mySidenav").style.width = "250px";
+// }
+
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+// }
